5e0155ebdc3b53786421df0205d73f0a33693eb5
Yevhen_Mosiichuk <yevhen.mosiichyk@gmail.com>
2020-08-03 22:32:44 +0300

    Updates

---
+++ precache-manifest.62819ff4618bc9b10c129692e40819e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71eedc01fd5670d018f023dc077ab77",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "3c7ab0f1daa4500ca261",
+    "url": "/react-todo/static/css/main.8fccd3f4.chunk.css"
+  },
+  {
+    "revision": "e3b35bae9e68898d271d",
+    "url": "/react-todo/static/js/2.64cc6350.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/react-todo/static/js/2.64cc6350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7ab0f1daa4500ca261",
+    "url": "/react-todo/static/js/main.ded97eb5.chunk.js"
+  },
+  {
+    "revision": "cfa8152b93379cf365f9",
+    "url": "/react-todo/static/js/runtime-main.eac2ca12.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-todo/static/media/logo.ee7cd8ed.svg"
+  }
+]);
